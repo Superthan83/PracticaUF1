63c38515efba150fe16c76beadac50b39d4eefb7
Superthan83 <146426677+Superthan83@users.noreply.github.com>
2023-11-17 18:39:37 +0100

    subo pdf actividad sin .exe porque no hubo manera.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+        private static void main (string[] args)
+        {
+            Console.Writeline("Hello, world!")
+        }
+}
+// Hola Marc, estuve mucho rato pero no hubo manera di de correr un hello world, no se que pasa, deberia de salir está todo, lo hice tal cual como en los apuntes que tomé
+// en clase peró nada :(
